78910d80f3cc19528177ef9ae97e8c8db4a2efb7
ankitjt <48979824+ankitjt@users.noreply.github.com>
2025-05-21 16:14:48 +0530

    Add files via upload

--- scripts/common.js
+++ scripts/common.js
@@ -1,7 +1,31 @@
 let siteNavbar = document.querySelector( ".site-navbar" );
 let siteLinks = document.querySelectorAll( ".site-link" );
-let pageLinks = document.querySelectorAll( ".nav-link" );
+let pageLinks = document.querySelectorAll( ".page-link" );
 let hideSiteNav = document.querySelector( ".hideSiteNav" );
-let showSiteNav = document.querySelector( ".showSiteNav" )
+let showSiteNav = document.querySelector(".showSiteNav")
 
+showSiteNav.onclick = () =>
+    {
+      siteNavbar.classList.add( "left-0", "z-10" );
+      siteNavbar.classList.remove( "-left-[100vw]" );
+    };
+    
+    hideSiteNav.onclick = () =>
+    {
+      siteNavbar.classList.add( "-left-[100vw]" );
+      siteNavbar.classList.remove( "left-0" );
+    };
+
+const currentPath = window.location.pathname.split("/").pop()
+siteLinks.forEach(site_link => {
+    let site_url = site_link.getAttribute("href").split("/").pop()
+    if (currentPath === site_url) {
+        site_link.classList.add("text-white")
+    }
+    site_link.onclick = () => {
+        if (site_url === currentPath) {
+            site_link.classList.add("text-white")
+        }
+    }
+})
 
--- scripts/expenses.js
+++ scripts/expenses.js
@@ -1,25 +1,88 @@
-showSiteNav.onclick = () =>
-{
-  siteNavbar.classList.add( "left-0", "z-10" );
-  siteNavbar.classList.remove( "-left-[100vw]" );
-};
+let ex_sections = document.querySelectorAll(".ex-sections")
+let exAddButtons = document.querySelectorAll(".ex-addButtons")
 
-hideSiteNav.onclick = () =>
-{
-  siteNavbar.classList.add( "-left-[100vw]" );
-  siteNavbar.classList.remove( "left-0" );
-};
-
-
-pageLinks.forEach( page =>
-{
-  page.onclick = () =>
+pageLinks.forEach(page =>
   {
-    pageLinks.forEach( link =>
-    {
-      link.classList.remove( "text-white" );
-    } );
-    page.classList.add( "text-white" );
+      page.onclick = () =>
+      {
+      pageLinks.forEach( link =>
+      {
+          link.classList.remove( "text-white" );
+      } );
+        page.classList.add("text-white");
+        ex_sections.forEach(section => {
+          section.classList.add("-left-[100vw]")
+          section.classList.remove("left-0")
+          window.scrollTo({top: 0})
+          if (page.dataset.name === section.dataset.name) {
+            section.classList.remove("-left-[100vw]")
+            section.classList.add("left-0")
+          }
+        })
+      };
+});
 
-  };
-} );
+// Add button actions 
+exAddButtons.forEach(addButton => {
+  addButton.onclick = () => {
+    if (addButton.dataset.button === "addExpense") {
+      expenseInputs()
+    }
+    if (addButton.dataset.button === "addBaseAmount") {
+      console.log("Add Base Amount button clicked.")
+    }
+    // ex_sections.forEach(section => {
+    //   section.classList.add("-left-[100vw]")
+    //   section.classList.remove("left-0")
+    //   if (section.dataset.name === "ledger") {
+    //     section.classList.add("left-0")
+    //     section.classList.remove("-left-[100vw]")
+    //   }
+    // })
+    // pageLinks.forEach(page => {
+    //   page.classList.remove("text-white")
+    //   if (page.dataset.name === "ledger") {
+    //     page.classList.add("text-white")
+    //   }
+    // })
+  }
+})
+
+const expenseInputs = () => {
+
+  let expenseInputs = document.querySelectorAll(".ex-expenseInputs")
+  let allFilled = true
+
+  expenseInputs.forEach(input => {
+    let header = input.parentElement.children[ 0 ]
+    let requiredTextClass = header.querySelector(".required-text")
+
+    if (requiredTextClass) {
+      header.removeChild(requiredTextClass)
+    }
+
+    if (input.value === "") {
+      allFilled = false
+      input.classList.add('border-2', 'border-rose-600')
+      let requiredText = document.createElement("span")
+      requiredText.classList.add("required-text")
+      requiredText.innerHTML = `<span class="text-rose-600">Required</span>`
+      header.appendChild(requiredText)
+    }
+    else {
+      input.classList.remove("border", "border-rose-600")
+    }
+  })
+
+  if (allFilled) {
+    expenseInputs.forEach(input => {
+      console.log(input.getAttribute("id"), input.value)
+      input.value = ""  
+    })
+  }
+}
+
+
+
+
+
